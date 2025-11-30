fd454b146d81ab0d2f6ddf39eca91cb9c9e7271a
HeZonglin <hezonglin657@gmail.com>
2025-11-30 12:06:46 +0800

    optuna best hparams in FD 2-3 99.64

--- scripts/run_optuna_on_server.py
+++ scripts/run_optuna_on_server.py
@@ -38,7 +38,7 @@ STUDY_DB = REPO_ROOT / "optuna.db"
 # Search scenarios and shared options -----------------------------------------
 # ------------------------------------------------------------------------------
 SCENARIO_GROUPS: List[List[Dict[str, int]]] = [
-[{"src": 1, "trg": 2}],
+[{"src": 2, "trg": 3}],
 ]
 
 DEFAULT_N_TRIALS = 80
